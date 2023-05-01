70a0496752a4de72ab2eef22c990bcd8d1338691
philip-loeffler <philiploeffler@gmail.com>
2023-05-01 13:43:21 -0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
